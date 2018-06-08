865383c52b17572346535930156a7aeac2f78f88
Shane Todhunter <shane@klue.com>
2018-06-08 09:49:49 -0700

    Added in favicon

--- html/wp-content/themes/klue/header.php
+++ html/wp-content/themes/klue/header.php
@@ -49,7 +49,22 @@ https://angel.co/klue
 
 <script src="https://use.typekit.net/xui2xju.js"></script>
 <script>try{Typekit.load({ async: true });}catch(e){}</script>
-
+  <!-- Icons -->
+        <meta name="msapplication-TileColor" content="#414143" />
+        <meta name="msapplication-TileImage" content="https://klue.com/content/themes/klue/assets/images/favicon/ms-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="57x57" href="https://klue.com/content/themes/klue/assets/images/favicon/apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="https://klue.com/content/themes/klue/assets/images/favicon/apple-icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="https://klue.com/content/themes/klue/assets/images/favicon/apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="https://klue.com/content/themes/klue/assets/images/favicon/apple-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="https://klue.com/content/themes/klue/assets/images/favicon/apple-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="https://klue.com/content/themes/klue/assets/images/favicon/apple-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="https://klue.com/content/themes/klue/assets/images/favicon/apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="https://klue.com/content/themes/klue/assets/images/favicon/apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="https://klue.com/content/themes/klue/assets/images/favicon/apple-icon-180x180.png" />
+        <link rel="icon" type="image/png" sizes="192x192"  href="https://klue.com/content/themes/klue/assets/images/favicon/android-icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="https://klue.com/content/themes/klue/assets/images/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="https://klue.com/content/themes/klue/assets/images/favicon/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="https://klue.com/content/themes/klue/assets/images/favicon/favicon-16x16.png" />
 </head>
 <body <?php body_class(); ?>>
   <!--[if lt IE 8]>
