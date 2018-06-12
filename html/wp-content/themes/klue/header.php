333288eb8e287d91c919c1fd6ec38869c51752f9
Shane Todhunter <shane@klue.com>
2018-06-12 13:41:36 -0700

    Added arrows

--- html/wp-content/themes/klue/header.php
+++ html/wp-content/themes/klue/header.php
@@ -92,8 +92,8 @@ https://angel.co/klue
 
     <?php if (/* blog section */is_home() || is_category() || is_post_type_archive('post') || is_singular('post')) { ?>
       <div class="blognav">
-        <span class="catlist__cover catlist__cover--front"></span>
-        <span class="catlist__cover catlist__cover--back"></span>
+        <span class="catlist__cover catlist__cover--front"><<</span>
+        <span class="catlist__cover catlist__cover--back">>></span>
         <ul class="blognav__catlist">
           <?php
             $cat_arguments = array(
