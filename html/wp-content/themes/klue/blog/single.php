3ffeda4c0b812c60d959709506ebc79ca0292678
Shane Todhunter <shane@klue.com>
2018-06-20 13:01:28 -0700

    Tweaks

--- html/wp-content/themes/klue/blog/single.php
+++ html/wp-content/themes/klue/blog/single.php
@@ -58,7 +58,7 @@ define("TITLE", "Klue | " . get_the_title());
 
 <section class="related">
 	<a class="button button--green button--blogitems" href="/blog/">Back to Blog</a>
-	<a href="#" class="button button--green button--subscribe button--subscribe-blog">Subscribe</a>
+	<a href="#" class="button button--green button--subscribe button--blogitems">Subscribe</a>
 	<h1 class="blogitems__heading">Related Posts</h1>
 
 	<div class="blogitems">
