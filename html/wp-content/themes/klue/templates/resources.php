9889d1e6a1fc509024ea62a4bf4cfbcc33d3b156
Shane Todhunter <shane@klue.com>
2018-06-27 16:50:49 -0700

    hide play button if not webinar

--- html/wp-content/themes/klue/templates/resources.php
+++ html/wp-content/themes/klue/templates/resources.php
@@ -22,7 +22,9 @@ define("DESCRIPTION", "Learn More about Klue and Competitive Intelligence");
 
             <li class="<?php if(get_sub_field('resource_type') == "Document"): ?>resources__item<?php else: ?>webinars__item<?php endif; ?>">
               <?php if(get_sub_field('resource_title')): ?><h1 class="section__heading"><?php the_sub_field('resource_title'); ?></h1><?php endif; ?>
-              <?php if(get_sub_field('resource_label')): ?><a class="button button--webinar button--green-solid" modal-data="" href="#" <?php $resource_file = get_sub_field('resource_file'); $resource_link = get_sub_field('resource_link'); if( $resource_file ): ?>data-location="<?php echo $resource_file; ?>" <?php elseif( $resource_link ): ?>data-location="<?php echo $resource_link; ?>" <?php endif; ?>><i class="fa fa-play-circle" aria-hidden="true"></i><?php the_sub_field('resource_label'); ?></a><?php endif; ?>
+              <?php if(get_sub_field('resource_label')): ?><a class="button button--webinar button--green-solid" modal-data="" href="#" <?php $resource_file = get_sub_field('resource_file'); $resource_link = get_sub_field('resource_link'); if( $resource_file ): ?>data-location="<?php echo $resource_file; ?>" <?php elseif( $resource_link ): ?>data-location="<?php echo $resource_link; ?>" <?php endif; ?>>
+                <?php if(the_sub_field('resource_label') == "Watch Webinar") { ?><i class="fa fa-play-circle" aria-hidden="true"></i><?php } ?>
+                <?php the_sub_field('resource_label'); ?></a><?php endif; ?>
               <?php if(get_sub_field('resource_description')): ?><?php the_sub_field('resource_description'); ?><?php endif; ?>
             </li>
           <?php endwhile; ?>
