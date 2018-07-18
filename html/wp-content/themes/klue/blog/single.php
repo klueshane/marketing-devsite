e513d45ed741c2b3ec9d54ca98561a9fbc294666
Shane Todhunter <shane@klue.com>
2018-07-18 12:21:29 -0700

    text change on subscribe button.

--- html/wp-content/themes/klue/blog/single.php
+++ html/wp-content/themes/klue/blog/single.php
@@ -57,7 +57,7 @@ define("TITLE", "Klue | " . get_the_title());
 
 <section class="related">
 	<a class="button button--green button--blogitems" style="margin-left:0;" href="/blog/">Back to Blog</a>
-	<a href="#" class="button button--green button--subscribe button--blogitems" style="margin-left:0;">Subscribe</a>
+	<a href="#" class="button button--green button--subscribe button--blogitems" style="margin-left:0;">Subscribe to Blog</a>
 	<h1 class="blogitems__heading">Related Posts</h1>
 
 	<div class="blogitems">
