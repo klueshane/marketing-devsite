e4f3c5c2d3c99649b37e21e4f5594687f8d9b2e5
Shane Todhunter <shane@klue.com>
2018-07-24 14:15:40 -0700

    testing

--- html/file-downloader.php
+++ html/file-downloader.php
@@ -8,12 +8,18 @@ $allowedFiles = array(
 	"http://fmq.a52.mwp.accessdomain.com/wp-content/uploads/2018/06/Klue-Ebook-Bundle-for-Product-Marketers.pdf",
 	"http://fmq.a52.mwp.accessdomain.com/wp-content/uploads/2018/02/Klue-Ebook-Bundle-for-Product-Marketers.pdf",
 );
-	if(in_array($file, $allowedFiles)) {
+echo(in_array($file, $allowedFiles));
+if(in_array($file, $allowedFiles)) {
 	$pathinfo = parse_url($file);
 	header("Content-Description: File Transfer"); 
 	header("Content-Type: application/octet-stream"); 
 	header("Content-Disposition: attachment; filename='" . basename($pathinfo['path']) . "'"); 
 	readfile ($_SERVER['DOCUMENT_ROOT'].$pathinfo['path']);
+}
+else {
+	header('HTTP/1.0 403 Forbidden');
+
+	echo 'Forbidden Activity';
 }
 exit(); 
 ?>
