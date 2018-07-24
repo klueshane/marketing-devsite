27039d2bd7409271436531a860735731624d8f17
Shane Todhunter <shane@klue.com>
2018-07-24 14:09:33 -0700

    Added in hardcoded list of allowed files

---
+++ html/file-downloader.php
@@ -0,0 +1,22 @@
+<?php
+//$file = $_POST['fileName']; 
+$file = $_GET['fileName']; 
+
+// hardcode list of allowed files to prevent abuse
+
+$allowedFiles = array(
+	"http://fmq.a52.mwp.accessdomain.com/wp-content/uploads/2018/06/Klue-Ebook-Bundle-for-Product-Marketers.pdf",
+	"http://fmq.a52.mwp.accessdomain.com/wp-content/uploads/2018/02/Klue-Ebook-Bundle-for-Product-Marketers.pdf",
+);
+in_array($file, $allowedFiles) {
+	$pathinfo = parse_url($file);
+	header("Content-Description: File Transfer"); 
+	header("Content-Type: application/octet-stream"); 
+	header("Content-Disposition: attachment; filename='" . basename($pathinfo['path']) . "'"); 
+	readfile ($_SERVER['DOCUMENT_ROOT'].$pathinfo['path']);
+}
+exit(); 
+?>
+
+
+ 
