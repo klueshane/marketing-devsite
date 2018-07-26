6a2108ddeb4f879a389124ebef4a96a7c996a5fb
Shane Todhunter <shane@klue.com>
2018-07-26 10:39:25 -0700

    bug fix for team page.

--- html/wp-content/themes/klue/assets/js/main.js
+++ html/wp-content/themes/klue/assets/js/main.js
@@ -35,7 +35,7 @@ var ninjaFormsResourcesListener = Marionette.Object.extend({
 
   $('.modal__closer').click(function (event) {
     event.preventDefault();
-    $(this).parent().toggle();
+    $(this).parents().find('.modal').toggle();
 
   });
 
