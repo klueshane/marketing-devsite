1023731826b627495fcf754b17abbb22d38b4399
Shane Todhunter <shane@klue.com>
2018-08-23 12:03:32 -0700

    Remove text from bottom. and make video modal correct size

--- html/wp-content/themes/klue/footer.php
+++ html/wp-content/themes/klue/footer.php
@@ -63,7 +63,6 @@
   <div class="modal__container">
     <div class="modal__info">
     <iframe src="https://player.vimeo.com/video/286260071" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-<p><a href="https://vimeo.com/286260071">Klue Overview: Competitive Intelligence Platform</a> from <a href="https://vimeo.com/user70942503">Katie @ Klue</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
   </div><a class="modal__close" href="">Close</a>
   </div>
 </section>
