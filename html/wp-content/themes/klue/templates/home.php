7ea36abfaf85b761981c4050e2e1e569b0048787
Shane Todhunter <shane@klue.com>
2018-11-15 13:15:50 -0800

    testing

--- html/wp-content/themes/klue/templates/home.php
+++ html/wp-content/themes/klue/templates/home.php
@@ -7,7 +7,7 @@ https://angel.co/klue
 
 -->
 <!doctype html>
-<html class="no-js" lang="">
+<html class="no-js" lang="en">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
