6687cdda4a6008f56b30ac430ed6a94315ed21b4
Shane Todhunter <shane@klue.com>
2018-11-23 12:56:27 -0800

    Added in FB Pixel code

--- html/wp-content/themes/klue/templates/home.php
+++ html/wp-content/themes/klue/templates/home.php
@@ -67,6 +67,26 @@ https://angel.co/klue
           a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
         </script>
         <!-- Global site tag (gtag.js) - Google Ads: 925652625 --> <script async src="https://www.googletagmanager.com/gtag/js?id=AW-925652625"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-925652625'); </script>
+
+        <!-- Facebook Pixel Code -->
+        <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window,document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+         fbq('init', '376052022940493'); 
+        fbq('track', 'PageView');
+        </script>
+        <noscript>
+         <img height="1" width="1" 
+        src="https://www.facebook.com/tr?id=376052022940493&ev=PageView
+        &noscript=1"/>
+        </noscript>
+<!-- End Facebook Pixel Code -->
     </head>
     <body class="page--home">
 
