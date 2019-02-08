53608c74d97e5c94f1c56be396d47ea7ef851aef
Shane Todhunter <shane@klue.com>
2019-02-08 12:53:45 -0800

    Debugging

--- html/wp-content/themes/klue/templates/about.php
+++ html/wp-content/themes/klue/templates/about.php
@@ -44,6 +44,7 @@
     <h1 class="heading heading--awards">Our Awards</h1>
     <p>Here's a peak at Klue's trophy self. We are humbled to have been selected as the recipient of so many prestigious awards.</p>
    <?php $images = get_field('awards');
+   var_dump($images);
 
   if( $images ): ?>
     <ul>
