3b24abab7293811496ac817ffb3fb092b5b156dc
Shane Todhunter <shane@klue.com>
2019-02-13 15:02:28 -0800

    Update file-downloader.php

--- html/file-downloader.php
+++ html/file-downloader.php
@@ -7,12 +7,12 @@ $referer = parse_url($_SERVER["HTTP_REFERER"]);
 // hardcode list of allowed files to prevent abuse
 
 $allowedFiles = array(
-	"/wp-content/uploads/2018/06/Klue-Ebook-Bundle-for-Product-Marketers.pdf",
-	"/wp-content/uploads/2018/02/Klue-Ebook-Bundle-for-Product-Marketers.pdf",
-	"/wp-content/uploads/2018/07/06-25-2018_Klue-Battlecard-Examples-1.pdf",
-	"/wp-content/uploads/2018/07/WinLoss_Interview_Checklist_Klue-1.pdf",
-	"/wp-content/uploads/2018/07/K2-Case-Study-May-7.pdf",
-	"/wp-content/uploads/2018/07/Klue-Case-Study-Dell.pdf",
+	"/wp-content/uploads/2019/02/Klue-Ebook-Bundle-for-Product-Marketers.pdf",
+	"/wp-content/uploads/2019/02/Klue-Ebook-Bundle-for-Product-Marketers.pdf",
+	"/wp-content/uploads/2019/02/06-25-2018_Klue-Battlecard-Examples-1.pdf",
+	"/wp-content/uploads/2019/02/WinLoss_Interview_Checklist_Klue-1.pdf",
+	"/wp-content/uploads/2019/02/K2-Case-Study-May-7.pdf",
+	"/wp-content/uploads/2019/02/Klue-Case-Study-Dell.pdf",
 );
 // check if query param fileName is in the list of approved downloads
 // check if referer is ok
