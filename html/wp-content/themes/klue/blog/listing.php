c6f08147e1a986e1fda6c2d0d8886363b1afeb4b
Shane Todhunter <shane@klue.com>
2019-02-13 12:14:36 -0800

    Update listing.php

--- html/wp-content/themes/klue/blog/listing.php
+++ html/wp-content/themes/klue/blog/listing.php
@@ -20,7 +20,7 @@
                   <div class="blogfeatured__text">
                     <p class="blogfeatured__category">
 <?php $primary_cat_id=get_post_meta(get_the_ID(),'_yoast_wpseo_primary_product_cat',true);
-var_dump(get_the_ID());
+var_dump($primary_cat_id);
 if($primary_cat_id){
    $product_cat = get_term($primary_cat_id, 'product_cat');
    if(isset($product_cat->name)) 
